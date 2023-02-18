97450da36cb5f581aa35d976baa867f6553b9ca0
NehaAnnreddy9 <98866453+NehaAnnreddy9@users.noreply.github.com>
2023-02-18 18:18:01 -0500

    Optimized bubble sort

--- Algorithms/BubbleSort.py
+++ Algorithms/BubbleSort.py
@@ -1,14 +1,12 @@
-def swap(arr, j, k):
-    temp = arr[j]
-    arr[j] = arr[k]
-    arr[k] = temp
-    
 def Bubble_sort(arr):
     size = len(arr)
     if  size == 1: return arr
     for i in range(size-1, 0, -1) :
+        swapped = False
         for j in range(i):
             if arr[j+1] < arr[j]:
-                swap(arr, j, j+1)
-    
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+        if swapped == False:
+            break
     return arr
