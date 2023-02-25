3f8fc771f35ae2efde6524835e9f7b88f2b07867
Neha Annreddy <98866453+NehaAnnreddy9@users.noreply.github.com>
2023-02-25 18:00:04 -0500

    Backtracking solution added for rat in maze

--- Algorithms/Backtracking Problems/RatInMaze.py
+++ Algorithms/Backtracking Problems/RatInMaze.py
@@ -13,5 +13,28 @@ def rat_in_maze(arr,i,j):
         print(i,j)
         return True
     else: return False
+    
+#Bactracking solution to print path from start to finish-        
+def isSafe(arr,i,j):
+    if i<len(arr) and j<len(arr) and arr[i][j] == 1:
+        return True
         
-print(rat_in_maze([[1,0,0,0],[1,1,0,1],[1,0,0,0],[1,1,1,1]],0,0))
+def rat_in_maze(arr):
+    n = len(arr)
+    sol = [[0 for x in range(n)] for x in range(n)]
+    if rat_in_maze_rec(arr,0,0,sol) == True:
+        print(sol)
+        return True
+    else:
+        return False
+    
+def rat_in_maze_rec(arr,i,j,sol):
+    if i == j and i == len(arr) - 1:
+        sol[i][j] = 1
+        return True
+    if isSafe(arr,i,j) == True:   
+        sol[i][j] = 1
+        if rat_in_maze_rec(arr,i+1,j,sol) == True: return True 
+        elif rat_in_maze_rec(arr,i,j+1,sol) == True: return True
+        sol[i][j] = 0
+    return False
